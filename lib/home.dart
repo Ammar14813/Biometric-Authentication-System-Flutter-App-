6ab156a0cb4504d44c562ff7c2f961a4ad55f80c
Braykaa Abdelrazek Ahmed <52169899+Ammar14813@users.noreply.github.com>
2021-07-05 19:37:45 +0200

    OTP System

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,16 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:io' as io;
 
-import 'package:audioplayers/audioplayers.dart';
 import 'package:file/local.dart';
+import 'package:final_project/recorder/feature_buttons_view.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:final_project/flutter_audio_recorder.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   final LocalFileSystem localFileSystem;
+
+
 
   Home({localFileSystem})
       : this.localFileSystem = localFileSystem ?? LocalFileSystem();
@@ -21,25 +20,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   File _file;
-  Future pickercamera() async{
-    final image = await ImagePicker().getImage(source: ImageSource.camera);
-    setState(() {
-      _file = File(image.path);
-    });
-  }
-
-
-  FlutterAudioRecorder _recorder;
-  Recording _current;
-  RecordingStatus _currentStatus = RecordingStatus.Unset;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _init();
-  }
-
+  List<Reference> references;
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -97,61 +79,9 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         SizedBox(width: 5,),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 35),
-                          child: Row(
-                            children: [
-                              new FlatButton(
-                                onPressed: () {
-                                  switch (_currentStatus) {
-                                    case RecordingStatus.Initialized:
-                                      {
-                                        _start();
-                                        break;
-                                      }
-                                    case RecordingStatus.Recording:
-                                      {
-                                        _pause();
-                                        break;
-                                      }
-                                    case RecordingStatus.Paused:
-                                      {
-                                        _resume();
-                                        break;
-                                      }
-                                    case RecordingStatus.Stopped:
-                                      {
-                                        _init();
-                                        break;
-                                      }
-                                    default:
-                                      break;
-                                  }
-                                },
-                                child: _buildText(_currentStatus),
-                                color: Colors.lightBlue,
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              new FlatButton(
-                                onPressed:
-                                _currentStatus != RecordingStatus.Unset ? _stop : null,
-                                child:
-                                new Text("Stop", style: TextStyle(color: Colors.white)),
-                                color: Colors.blueAccent.withOpacity(0.5),
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              new FlatButton(
-                                onPressed: onPlayAudio,
-                                child:
-                                new Text("Play", style: TextStyle(color: Colors.white)),
-                                color: Colors.blueAccent.withOpacity(0.5),
-                              ),
-                            ],
-                          ),
+
+                        FeatureButtonsView(
+                          onUploadComplete: _onUploadComplete,
                         ),
 
                         SizedBox(height: 25,),
@@ -179,7 +109,10 @@ class _HomeState extends State<Home> {
                   RaisedButton(
                     child: Text('Verify',style: theme1,),
                     color: Colors.black,
-                    onPressed: (){},
+                    onPressed: (){
+                      _uploadData(_file);
+                      _onUploadComplete();
+                    },
                   ),
                 ],
               ),
@@ -189,127 +122,49 @@ class _HomeState extends State<Home> {
     );
   }
 
-  _init() async {
-    try {
-      if (await FlutterAudioRecorder.hasPermissions) {
-        String customPath = '/flutter_audio_recorder_';
-        io.Directory appDocDirectory;
-//        io.Directory appDocDirectory = await getApplicationDocumentsDirectory();
-        if (io.Platform.isIOS) {
-          appDocDirectory = await getApplicationDocumentsDirectory();
-        } else {
-          appDocDirectory = await getExternalStorageDirectory();
-        }
-
-        // can add extension like ".mp4" ".wav" ".m4a" ".aac"
-        customPath = appDocDirectory.path +
-            customPath +
-            DateTime.now().millisecondsSinceEpoch.toString();
-
-        // .wav <---> AudioFormat.WAV
-        // .mp4 .m4a .aac <---> AudioFormat.AAC
-        // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
-        _recorder =
-            FlutterAudioRecorder(customPath, audioFormat: AudioFormat.WAV);
-
-        await _recorder.initialized;
-        // after initialization
-        var current = await _recorder.current(channel: 0);
-        print(current);
-        // should be "Initialized", if all working fine
-        setState(() {
-          _current = current;
-          _currentStatus = current.status;
-          print(_currentStatus);
-        });
-      } else {
-        Scaffold.of(context).showSnackBar(
-            new SnackBar(content: new Text("You must accept permissions")));
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  _start() async {
-    try {
-      await _recorder.start();
-      var recording = await _recorder.current(channel: 0);
-      setState(() {
-        _current = recording;
-      });
-
-      const tick = const Duration(milliseconds: 50);
-      new Timer.periodic(tick, (Timer t) async {
-        if (_currentStatus == RecordingStatus.Stopped) {
-          t.cancel();
-        }
-
-        var current = await _recorder.current(channel: 0);
-        // print(current.status);
-        setState(() {
-          _current = current;
-          _currentStatus = _current.status;
-        });
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  _resume() async {
-    await _recorder.resume();
-    setState(() {});
-  }
-
-  _pause() async {
-    await _recorder.pause();
-    setState(() {});
-  }
-
-  _stop() async {
-    var result = await _recorder.stop();
-    print("Stop recording: ${result.path}");
-    print("Stop recording: ${result.duration}");
-    File file = widget.localFileSystem.file(result.path);
-    print("File length: ${await file.length()}");
+  Future<void> _onUploadComplete() async {
+    FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+    ListResult listResult =
+    await firebaseStorage.ref().child('upload-voice-firebase').list();
     setState(() {
-      _current = result;
-      _currentStatus = _current.status;
+      references = listResult.items;
+      print(references);
     });
   }
 
-  Widget _buildText(RecordingStatus status) {
-    var text = "";
-    switch (_currentStatus) {
-      case RecordingStatus.Initialized:
-        {
-          text = 'Start';
-          break;
-        }
-      case RecordingStatus.Recording:
-        {
-          text = 'Pause';
-          break;
-        }
-      case RecordingStatus.Paused:
-        {
-          text = 'Resume';
-          break;
-        }
-      case RecordingStatus.Stopped:
-        {
-          text = 'Init';
-          break;
-        }
-      default:
-        break;
+  Future pickercamera() async {
+    final image = await ImagePicker().getImage(
+      source: ImageSource.camera,
+    );
+    if (image != null) {
+      setState(() {
+        _file = File(image.path);
+      });
+      print("Done!");
+    } else {
+      print("no image selected");
     }
-    return Text(text, style: TextStyle(color: Colors.white));
   }
 
-  void onPlayAudio() async {
-    AudioPlayer audioPlayer = AudioPlayer();
-    await audioPlayer.play(_current.path, isLocal: true);
+  void _uploadData(File image) async {
+    //try to upload the image
+    try {
+      print("1");
+      //make a reference have two child the first one for file name, second for image name
+      final ref =
+      FirebaseStorage.instance.ref().child('clint_images').child('1.jpg');
+      //upload the image into the server
+      print("2");
+      await ref.putFile(image);
+      print("3");
+      setState(() {
+        _isLoading = false;
+        print("Image uploaded successfully");
+      });
+    }
+    //if an error occurred try to know the reason of error and handle it
+    catch (e) {
+      print(e);
+    }
   }
 }
